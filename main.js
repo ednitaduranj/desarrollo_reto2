4d8cc45095bc0c5b8b5ea96c0c865753030469b9
Johan Vargas <pvargas@hugeinc.com>
2021-10-07 21:57:00 -0500

    agregamos metodos para ver e insertar valores a la base de datos.

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
-function registro(){
-  var registro={
-    id:$("#miId"). val(),
-    name:$("#name"). val(),
-    email:$("#email"). val(),
-    age:$("#age"). val(),
+let registro = {
+  id: 0,
+  name: '',
+  email: '',
+  age: 0
+}
+
+const URL = 'https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client';
+
+function setRegistro(){
+  registro = {
+    id: document.getElementById('id').value,
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    age: document.getElementById('age').value,
   }
-
-
-var dataToSend=JSON.stringify(registro);
-
-  $.ajax({
-      dataType:'json',
-      data:registro,
-      url: "https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
-      type:'POST',
-
-      success:function(response) {
-        console.log(response);
-      },
-
-      error:function(jqXHR, textStatus, errorThrown){
-
-      }            
-});
-
+  console.log(registro);
 }
 
 function obtenerItems(){
-  $.ajax({
-    dataType:'json',
-    data:registro,
-    url: "https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
-    type:'GET',
-
-    success:function(response) {
-      console.log(response);
-    },
-
-    error:function(jqXHR, textStatus, errorThrown)
-
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => console.log(data.items));
 }
 
+function insertItem() {
+  setRegistro();
+  fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify(registro),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response);
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+}
 
