3dc2ec6cde45ac111a7c14fd138515210ca55aeb
Johan Vargas <pvargas@hugeinc.com>
2021-10-07 22:03:43 -0500

    eliminar y actualizar desde front

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let registro = {
 
 const URL = 'https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client';
 
-function setRegistro(){
+function registrar(){
   registro = {
     id: document.getElementById('id').value,
     name: document.getElementById('name').value,
@@ -23,11 +23,51 @@ function obtenerItems(){
     .then(data => console.log(data.items));
 }
 
-function insertItem() {
-  setRegistro();
+function insertarItem() {
+  registrar();
   fetch(URL, {
     method: 'POST',
     body: JSON.stringify(registro),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response);
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+}
+
+function actualizarItem() {
+  registrar();
+  fetch(URL, {
+    method: 'PUT',
+    body: JSON.stringify(registro),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response);
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+}
+
+function eliminarItem() {
+  registrar();
+  fetch(URL, {
+    method: 'DELETE',
+    body: JSON.stringify({id: registro.id}),
     headers: {
       'Content-type': 'application/json; charset=UTF-8'
     }
