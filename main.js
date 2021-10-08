7bf741941f5fdd1c3b13c1f00ff26826dc0976c4
ednitaduranj <ednitaduranj@gmail.com>
2021-10-08 17:24:55 -0500

    Agrgue el formulario de Message

--- main.js
+++ main.js
@@ -18,6 +18,16 @@ function registrar(){
   console.log(registro);
 }
 
+function imprimirTabla(items) {
+  const tablaPrevia = document.getElementById('tabla');
+  let tabla = INICIO_TABLA;
+  items.forEach(item => {
+    const fila = '<tr><td>' + item.id + '</td><td>' + item.name + '</td><td>' + item.email  + '</td><td>' + item.age + '</td></tr>';
+    tabla = tabla + fila;
+  });
+  tablaPrevia.innerHTML = tabla;
+}
+
 function obtenerItems(){
   fetch(URL)
     .then(response => response.json())
@@ -61,6 +71,7 @@ function actualizarItem() {
   }).catch(function (error) {
     console.warn('Something went wrong.', error);
   });
+
 }
 
 function eliminarItem() {
@@ -82,14 +93,92 @@ function eliminarItem() {
   });
 }
 
-
-function imprimirTabla(items) {
-  const tablaPrevia = document.getElementById('tabla');
-  let tabla = INICIO_TABLA;
-  items.forEach(item => {
-    const fila = '<tr><td>' + item.id + '</td><td>' + item.name + '</td><td>' + item.email  + '</td><td>' + item.age + '</td></tr>';
-    tabla = tabla + fila;
-  });
-  tablaPrevia.innerHTML = tabla;
+let registroMensajes = {
+  id: 0,
+  messajeText: '',
 }
 
+const URL2='https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message';
+const MENSAJE_TABLA ='<tr><th>ID</th><th>MESSAGE</th>';
+
+function registrarMensaje(){
+  registroMensajes = {
+    id: document.getElementById('id').value,
+    messageText: document.getElementById('messageText').value,
+  }
+  console.log(registroMensajes);
+
+function imprimirMensajes(items) {
+  const tablaPreviaMensajes = document.getElementById('tablaMensaje');
+  let tablaMensaje = MENSAJE_TABLA;
+  items.forEach(item => {
+    const fila = '<tr><td>' + item.id + '</td><td>' + item.messageText + '</td></tr>';
+    tablaMensaje = tablaMensaje + fila;
+    });
+    tablaPreviaMensajes.innerHTML = tablaMensaje;
+  }
+function obtenerMensaje(){
+  fetch(URL2)
+    .then (response => response.json())
+    .then (data => imprimirMensajes(data.items));
+  }
+  
+function insertarMensaje(){
+  registrarMensaje();
+  fetch(URL2, {
+    method: 'POST',
+    body: JSON.stringify(registroMensajes),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      obtenerMensaje();
+      return response.json();
+    }
+    return Promise.reject(response);
+   }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+  
+  }
+
+function actualizarMensaje() {
+  registrarMensaje();
+  fetch(URL2, {
+    method: 'PUT',
+    body: JSON.stringify(registroMensajes),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      obtenerItems();
+       return response.json();
+    }
+    return Promise.reject(response);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+  
+}
+function eliminarMensaje() {
+  registrarMensaje();
+  fetch(URL, {
+    method: 'DELETE',
+    body: JSON.stringify({id: registroMensajes.id}),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      obtenerItems();
+      return response.json();
+    }
+    return Promise.reject(response);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+}
+  
+}
