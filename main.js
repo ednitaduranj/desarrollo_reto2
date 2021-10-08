aa30c095674953b94e3b5f0343e030e62b0246cc
ednitaduranj <ednitaduranj@gmail.com>
2021-10-07 20:16:45 -0500

    agregue formularios

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log('test USA software')
+function registro(){
+  var registro={
+    id:$("#miId"). val(),
+    name:$("#name"). val(),
+    email:$("#email"). val(),
+    age:$("#age"). val(),
+  }
+
+
+var dataToSend=JSON.stringify(registro);
+
+  $.ajax({
+      dataType:'json',
+      data:registro,
+      url: "https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
+      type:'POST',
+
+      success:function(response) {
+        console.log(response);
+      },
+
+      error:function(jqXHR, textStatus, errorThrown){
+
+      }            
+});
+
+}
+
+function obtenerItems(){
+  $.ajax({
+    dataType:'json',
+    data:registro,
+    url: "https://gff9b1b4fc9ed2f-reto2.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
+    type:'GET',
+
+    success:function(response) {
+      console.log(response);
+    },
+
+    error:function(jqXHR, textStatus, errorThrown)
+
+}
+
+
